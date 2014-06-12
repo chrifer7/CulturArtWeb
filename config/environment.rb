9364fa51da9f0e8133fd2d6d685424891cd82d19
chrifer7 <chrifer7@gmail.com>
2014-06-12 09:55:04 -0500

    pg3

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 RailsApp::Application.initialize!
 
-ENV['RAILS_ENV'] = 'development'
-#ENV['RAILS_ENV'] = 'production' 
+# ENV['RAILS_ENV'] = 'development'
+ENV['RAILS_ENV'] = 'production' 
 
